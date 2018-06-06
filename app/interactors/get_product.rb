072b9d1dc5bacaa2c2d1d8d8f82721e36211d724
Pavlo Dei <booogie@gmail.com>
2018-06-06 16:12:10 +0300

    clean up GetProduct interactor

--- app/interactors/get_product.rb
+++ app/interactors/get_product.rb
@@ -5,25 +5,16 @@ class GetProduct
   require 'open-uri'
 
   def call
-    puts "We here!!! #{context.inspect}"
     doc = Nokogiri::HTML(open(context.link))
     context.product_id = context.link[/\d+$/,0]
 
+    context.doc   = doc
     context.title = doc.css('h1.prod-ProductTitle div').first.content
-    context.price = doc.css('span.price-characteristic').first.attribute("content").value
-    context.doc = doc
+    context.price = doc.css('span.price-characteristic').first.attribute('content').value
 
-    # begin
-      # Product.create!(id:    context.product_id,
-      #                 title: context.title,
-      #                 price: context.price)
-      context.product = Product.where(id: context.product_id).first_or_create do |p|
-        p.title = context.title,
-        p.price = context.price
-      end
-    # rescue ActiveRecord::RecordNotUnique
-    #   # context.fail!(message: "db.record_not_unique")
-    #   Rails.logger.debug 'Product already exists in DB.'
-    # end
+    context.product = Product.where(id: context.product_id).first_or_create do |p|
+      p.title = context.title,
+      p.price = context.price
+    end
   end
 end
